b61411aca315cf9e44b8520ec28da094aa235cf3
kmw7855 <rrr7538@naver.com>
2021-11-29 13:34:59 +0900

    Update mygame.py

--- mygame.py
+++ mygame.py
@@ -1,11 +1,11 @@
 import game_framework
 import pico2d
 import test3
-#import start_state
+import start_state
 
 
 
 pico2d.open_canvas(1600, 1024)
-#game_framework.run(start_state)
-game_framework.run(test3)
+game_framework.run(start_state)
+#game_framework.run(test3)
 pico2d.close_canvas()
